843f2d56050c37c31ec9d0fdb210621633f54e96
javierv <vjavier.14q@gmail.com>
2021-06-15 18:18:44 -0500

    se quitó el loadingComponent

--- src/@helpers/router.js
+++ src/@helpers/router.js
@@ -1,23 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from "../components/Home.vue";
 import {authService} from "../@services/auth";
 
 import { ref, defineAsyncComponent } from "vue";
 const AsyncEdit = defineAsyncComponent({
-  loader: () => import("../components/Edit.vue" /* webpackChunckName: "Edit"*/),
-  loadingComponent: "<h1>CARGANDO...</h1>",
+  loader: () => import("../components/Edit.vue" /* webpackChunckName: "Edit"*/)
 })
 const AsyncLogin = defineAsyncComponent({
-  loader: () => import("../components/Login.vue" /* webpackChunckName: "Login"*/),
-  loadingComponent: "<h1>CARGANDO...</h1>",
+  loader: () => import("../components/Login.vue" /* webpackChunckName: "Login"*/)
 })
 const AsyncRegister = defineAsyncComponent({
-  loader: () => import("../components/Regiter.vue" /* webpackChunckName: "Register"*/),
-  loadingComponent: "<h1>CARGANDO...</h1>",
+  loader: () => import("../components/Regiter.vue" /* webpackChunckName: "Register"*/)
 })
 const AsyncHome = defineAsyncComponent({
-  loader: () => import("../components/Home.vue" /* webpackChunckName: "Home"*/),
-  loadingComponent: "<h1>CARGANDO...</h1>",
+  loader: () => import("../components/Home.vue" /* webpackChunckName: "Home"*/)
 })
 
 const routes = [
