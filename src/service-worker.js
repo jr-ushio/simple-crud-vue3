192a6ed2683dbd83f3153f30101be34712d80519
javierv <vjavier.14q@gmail.com>
2021-06-23 15:32:37 -0500

    separando GET y POST en sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -17,10 +17,37 @@ workbox.routing.registerRoute(navigationRoute);
   // console.log('data ', data);
 
   workbox.routing.registerRoute(
+    new RegExp('.*'), // Change to match your endpoint URL.
+    async ({event, url}) => {
+      event.respondWith(
+        // look in the cache for the resource
+        caches.match(event.request).then(async response => {
+          if (response) {
+            // is in cache, respond with the cached resource
+            return response;
+          }
+          // if not found fetch it from the network
+          const networkResponse = await fetch(event.request);
+          // response needs to be cloned if going to be used more than once
+          const clonedResponse = networkResponse.clone();
+
+          // save response to runtime cache for later use
+          const runtimeCache = await caches.open('runtime-cache');
+          runtimeCache.put(event.request, networkResponse);
+
+          // respond with the cloned network response
+          return Promise.resolve(clonedResponse);
+        })
+      );
+    },
+    'GET' // Opt-in to matching POST requests. Only GET is matched by default.
+  );
+
+  workbox.routing.registerRoute(
     'http://llega-ya.com/apitest/login', // Change to match your endpoint URL.
     async ({event, url}) => {
       const request = event.request;
-      if (false) {
+      if (true) {
         return new Response(JSON.stringify({"mensaje":"DEL SERVICE WORKER.","codigo":404,"data":null}), {
           headers: { 'Content-Type': 'text/json' }
         });
