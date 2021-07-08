e48cc48ad35fc8d548c595f08220517924245bdf
javierv <vjavier.14q@gmail.com>
2021-07-07 19:12:31 -0500

    cambiando request en offline

--- src/service-worker.js
+++ src/service-worker.js
@@ -13,85 +13,51 @@ workbox.routing.registerRoute(navigationRoute);
 (async () => {
   const myDB = await idb.openDB('SmartClicDB', 1);
 
-  // let data = await myDB.get('response', 1)
-  // console.log('data ', data);
-
-  // workbox.routing.registerRoute(
-  //   new RegExp('.*'), // Change to match your endpoint URL.
-  //   async ({event, url}) => {
-  //     event.respondWith(
-  //       // look in the cache for the resource
-  //       caches.match(event.request).then(async response => {
-  //         if (response) {
-  //           // is in cache, respond with the cached resource
-  //           return response;
-  //         }
-  //         // if not found fetch it from the network
-  //         const networkResponse = await fetch(event.request);
-  //         // response needs to be cloned if going to be used more than once
-  //         const clonedResponse = networkResponse.clone();
-  //
-  //         // save response to runtime cache for later use
-  //         const runtimeCache = await caches.open('runtime-cache');
-  //         runtimeCache.put(event.request, networkResponse);
-  //
-  //         // respond with the cloned network response
-  //         return Promise.resolve(clonedResponse);
-  //       })
-  //     );
-  //   },
-  //   'GET' // Opt-in to matching POST requests. Only GET is matched by default.
-  // );
-
   workbox.routing.registerRoute(
-    'http://llega-ya.com/apitest/login', // Change to match your endpoint URL.
+    /.*\/llega-ya.com.*/,
     async ({event, url}) => {
-      const request = event.request;
-      const value = await myDB.get('offline', 1);
-      if (!value || !value.status) {
-        // return new Response(JSON.stringify({"mensaje":"DEL SERVICE WORKER.","codigo":404,"data":null}), {
-        //   headers: { 'Content-Type': 'text/json' }
-        // });
-        return fetch(request.clone())
-      } else {
-        var req = new Request('http://llega-ya.com/apitest/login2', {
-          method: request.method,
-          headers: request.headers,
-          // mode: 'same-origin', // need to set this properly
-          credentials: request.credentials,
-          // redirect: 'manual'   // let browser handle redirects
-        });
-        return fetch(req)
-      }
-    },
-    'POST' // Opt-in to matching POST requests. Only GET is matched by default.
-  );
-
-  workbox.routing.registerRoute(
-    /.*\/apitest\/usuarios/, // Change to match your endpoint URL.
-    async ({event, url}) => {
-      const request = event.request;
-      const value = await myDB.get('offline', 1);
-      console.log('service worker');
-      if (!value || !value.status) {
-        return fetch(request.clone())
-      } else {
-        return new Response(JSON.stringify({
-          mensaje: '',
-          data: {
-            data: await myDB.getAllFromIndex('usuarios', 'id'),
-            lastPage: 1,
-            page: 1,
-            perPage: 1000,
-            total: 994
-          },
-          codigo: 200
-        }), {
-          headers: { 'Content-Type': 'text/json' }
-        });
-      }
+      return this.cambiarURL(myDB, event.request, url)
     },
     'GET'
   );
+  workbox.routing.registerRoute(
+    /.*\/llega-ya.com.*/,
+    async ({event, url}) => {
+      return this.cambiarURL(myDB, event.request, url)
+    },
+    'POST'
+  );
+  workbox.routing.registerRoute(
+    /.*\/llega-ya.com.*/,
+    async ({event, url}) => {
+      return this.cambiarURL(myDB, event.request, url)
+    },
+    'PUT'
+  );
+  workbox.routing.registerRoute(
+    /.*\/llega-ya.com.*/,
+    async ({event, url}) => {
+      return this.cambiarURL(myDB, event.request, url)
+    },
+    'DELETE'
+  );
+
 
 })();
+
+async function cambiarURL(myDB, request, url) {
+  const value = await myDB.get('offline', 1);
+  if (!value || !value.status) {
+    return fetch(request.clone())
+  } else {
+    const newUrl = url.href.replace('llega-ya.com', 'miniback.llega-ya.com')
+    const bodyP = request.headers.get('Content-Type') ? request.blob() : Promise.resolve(undefined);
+    let req = new Request(newUrl, {
+      method: request.method,
+      headers: request.headers,
+      credentials: request.credentials,
+      body: await bodyP
+    });
+    return fetch(req)
+  }
+}
